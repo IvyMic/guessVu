601ca4efd65da1169627bd98c7d15f5011ac618d
Sebastian <sebbecker@gmx.net>
2018-11-20 14:34:41 +0000

    set default color to white

--- frontend/src/alert.js
+++ frontend/src/alert.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Alert = (props) => {
+  console.log({"props of Alert": props});
   return(
     <div className="signupError" id="signupError">{props.msg}</div>
   )
